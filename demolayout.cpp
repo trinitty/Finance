06848ace6ce2357ae93d409a8b8ddb01296d95b3
Deepak Benny <trinitty@outlook.com>
2012-08-31 23:57:32 +0530

    UI screen

--- demolayout.cpp
+++ demolayout.cpp
@@ -7,11 +7,25 @@
  {
      
      int i;
+                 cout<<"\n\n\n\n\n";
+     cout<<"\t\t--------------------------------------------\n";
+     cout<<"\t\t|\t\t   STOCK\t           |\n";
+     cout<<"\t\t|   \t\t\t\t\t   |\n";
+     cout<<"\t\t|   \t\t\t\t\t   |\n";
+     cout<<"\t\t|\t1. Buy Stocks\t                   |\n";
+     cout<<"\t\t|   \t\t\t\t\t   |\n";
+     cout<<"\t\t|\t2. Sell Stocks\t                   |\n";
+     cout<<"\t\t|   \t\t\t\t\t   |\n";
+     cout<<"\t\t|\t3. PortFolio\t                   |\n";    
+     cout<<"\t\t|   \t\t\t\t\t   |\n";    
+     cout<<"\t\t|\t4. History\t                   |\n";
+     cout<<"\t\t|   \t\t\t\t\t   |\n";    
+      cout<<"\t\t--------------------------------------------\n";
      
-     cout<<"\t\t------------------------------------\n";
-     for(i=0;i<10;i++)
-     cout<<"|\n";
+     cout<<"\n\t\t\t#enter your option\n";
+     cout<<"\n\t\t\t>";
+     
+     
      
      getch();
-     
- }
+     }
