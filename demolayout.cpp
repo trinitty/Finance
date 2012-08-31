bb0ec26ab06469d021bd5793223626d145441d89
Deepak Benny <trinitty@outlook.com>
2012-08-31 21:32:53 +0530

    Just trying the table layout

---
+++ demolayout.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+ using namespace std;
+
+ int main()
+ {
+     
+     int i;
+     
+     cout<<"\t\t------------------------------------\n";
+     for(i=0;i<10;i++)
+     cout<<"|\n";
+     
+     getch();
+     
+ }
